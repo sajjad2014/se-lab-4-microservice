0da685fe5a0e609c40e108d237e34a4c290e4e42
sajjad2014 <sajjadshahabi20@yahoo.com>
2021-08-15 23:52:17 +0430

    add fail_count, timeout

--- api-gateway/controller.py
+++ api-gateway/controller.py
@@ -1,6 +1,7 @@
 from flask import Flask, json, Response, request
 from flask_restful import Resource, Api, reqparse, abort, marshal, fields
 import requests
+from requests.exceptions import Timeout
 from api_gateway_logic import APILogic
 
 apiLogic = APILogic()
@@ -21,12 +22,20 @@ class Signup(Resource):
         self.reqparse.add_argument(
             "mobile", type=str, required=True, help="The mobile number must be provided", location="json"
         )
-
+        self.fail_count = 0
 
     def post(self):
         args = self.reqparse.parse_args()
-        resp = requests.post("http://localhost:5001/signup", json=args)
-        return Response(resp.content, resp.status_code)  # todo header needed?
+        try:
+            resp = requests.post("http://localhost:5001/signup", json=args, timeout=0.5)
+        except Timeout:
+            self.fail_count += 1
+            #todo disable if needed
+            return Response(json.dumps({"error": "Connection Timed Out"}), 408)
+        if resp.content >= 500:
+            self.fail_count += 1
+            #todo disable if needed
+        return Response(resp.content, resp.status_code)
 
 
 class Login(Resource):
@@ -38,18 +47,37 @@ class Login(Resource):
         self.reqparse.add_argument(
             "password", type=str, required=True, help="The password must be provided", location="json"
         )
-
+        self.fail_count = 0
 
     def post(self):
         args = self.reqparse.parse_args()
-        resp = requests.post("http://localhost:5001/login", json=args)
+        try:
+            resp = requests.post("http://localhost:5001/login", json=args, timeout=0.5)
+        except Timeout:
+            self.fail_count += 1
+            #todo disable if needed
+            return Response(json.dumps({"error": "Connection Timed Out"}), 408)
+        if resp.content >= 500:
+            self.fail_count += 1
+            #todo disable if needed
         return Response(resp.content, resp.status_code)
 
 
 class ShowProf(Resource):
+    def __init__(self):
+        self.fail_count = 0
+
     def get(self):
         if apiLogic.is_valid(request.headers.get('token')):
-            resp = requests.get("http://localhost:5002/showprof", headers={'token': request.headers.get('token')})
+            try:
+                resp = requests.get("http://localhost:5002/showprof", headers={'token': request.headers.get('token')}, timeout=0.5)
+            except Timeout:
+                self.fail_count += 1
+                # todo disable if needed
+                return Response(json.dumps({"error": "Connection Timed Out"}), 408)
+            if resp.content >= 500:
+                self.fail_count += 1
+                # todo disable if needed
             return Response(resp.content, resp.status_code)
         Response('{"error": "invalid token"}', 401)
 
@@ -66,13 +94,21 @@ class UpdateProf(Resource):
         self.reqparse.add_argument(
             "mobile", type=str, required=True, help="The mobile number must be provided", location="json"
         )
-
+        self.fail_count = 0
 
     def post(self):
         args = self.reqparse.parse_args()
         if apiLogic.is_valid(request.headers.get('token')):
-            resp = requests.post("http://localhost:5002/updateprof", json=args,
-                                 headers={'token': request.headers.get('token')})
+            try:
+                resp = requests.post("http://localhost:5002/updateprof", json=args,
+                                     headers={'token': request.headers.get('token')}, timeout=0.5)
+            except Timeout:
+                self.fail_count += 1
+                # todo disable if needed
+                return Response(json.dumps({"error": "Connection Timed Out"}), 408)
+            if resp.content >= 500:
+                self.fail_count += 1
+                # todo disable if needed
             return Response(resp.content, resp.status_code)
         Response('{"error": "invalid token"}', 401)
 
