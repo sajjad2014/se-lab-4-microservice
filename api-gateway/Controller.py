7fa2117d9d69f9fa5a1a2008f1fbae5a056095f8
taslimisina <taslimisina@yahoo.com>
2021-08-15 19:01:50 +0430

    Add login to controller

--- api-gateway/Controller.py
+++ api-gateway/Controller.py
@@ -2,9 +2,6 @@ from flask import Flask, json, Response
 from flask_restful import Resource, Api, reqparse, abort, marshal, fields
 import requests
 
-app = Flask(__name__)
-api = Api(app)
-
 
 class Signup(Resource):
     def __init__(self):
@@ -29,7 +26,27 @@ class Signup(Resource):
         return Response(resp.content, resp.status_code)  # todo header needed?
 
 
+class Login(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument(
+            "username", type=str, required=True, help="The username must be provided", location="json"
+        )
+        self.reqparse.add_argument(
+            "password", type=str, required=True, help="The password must be provided", location="json"
+        )
+
+
+    def post(self):
+        args = self.reqparse.parse_args()
+        resp = requests.post("http://localhost:5001/login", json=args)
+        return Response(resp.content, resp.status_code)
+
+
+app = Flask(__name__)
+api = Api(app)
 api.add_resource(Signup, '/signup')
+api.add_resource(Login, '/login')
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
